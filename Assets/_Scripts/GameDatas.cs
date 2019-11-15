f43095a0504c6d351fce40df22f9b23c8dfe8d3e
Dangonok <dangonok@gmail.com>
2019-11-15 11:56:12 +0100

    scriptable object

---
+++ Assets/_Scripts/GameDatas.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameDatas", menuName = "GameDatas")]
+public class GameDatas : ScriptableObject
+{
+    public float playerSpeed;
+}
